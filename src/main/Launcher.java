8d771f5d88c9610ccb5b30c8124d4ff5ec6816d8
KarasuRou <Tjalf0000@gmx.de>
2021-10-01 12:37:27 +0200

    Kassierer-Aufgabe

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,7 +1,34 @@
 package main;
 
+import java.util.Scanner;
+
 public class Launcher {
-    public static void main(String[] args) {
+
+    final static double mws = 0.18; // percent
+
+    public static void main(String[] args){
+        double firstProduct, secondProduct, thirdProduct, fourthProduct, fifthProduct, sumOfProduct, mwsAmount;
+
+        System.out.println("Bitte geben sie 5 Werte im Folgenden Format ein (5,5):");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("1 Produkt > ");
+        firstProduct = scanner.nextDouble();
+        System.out.print("2 Produkt > ");
+        secondProduct = scanner.nextDouble();
+        System.out.print("3 Produkt > ");
+        thirdProduct = scanner.nextDouble();
+        System.out.print("4 Produkt > ");
+        fourthProduct = scanner.nextDouble();
+        System.out.print("5 Produkt > ");
+        fifthProduct = scanner.nextDouble();
+
+        sumOfProduct = firstProduct + secondProduct + thirdProduct + fourthProduct + fifthProduct;
+        mwsAmount = sumOfProduct * mws;
+
+        System.out.println("\r\n\r\nSumme aller Produkte: " + sumOfProduct + "€\r\n" +
+                "Anteil Mehrwertsteuer: " + mwsAmount + "€\r\n" +
+                "Anzahl der Produkte: 5");
 
     }
 }
