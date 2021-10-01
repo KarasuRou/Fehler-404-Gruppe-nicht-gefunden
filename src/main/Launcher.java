754ccf67e1b02a0d0272c97e2a94c90cbd00e3e5
Anthony <a.steding@hotmail.com>
2021-10-01 12:11:02 +0200

    Update Launcher.java

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,7 +1,17 @@
 package main;
-
 public class Launcher {
+    
     public static void main(String[] args) {
-
+        double article1;
+        double article2;
+        double article3;
+        double article4;
+        double article5;
+         double sum;
+        double vat;
+        Scanner scanner = new Scanner();
+        System.out.println("Bitte geben Sie den Preis der Produkte an.");
+        double sum = a1+a2+a3+a4+a5;
+        double 
     }
 }
