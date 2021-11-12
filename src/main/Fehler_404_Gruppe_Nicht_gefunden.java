5c1f73cd0cdb35968a530a8a580808ccca2ea3f6
KarasuRou <Tjalf0000@gmx.de>
2021-11-12 08:24:01 +0100

    Refactored code to new task. - new Feature (dynamic input)

--- src/main/Fehler_404_Gruppe_Nicht_gefunden.java
+++ src/main/Fehler_404_Gruppe_Nicht_gefunden.java
@@ -5,24 +5,20 @@ import java.util.Scanner;
 public class Fehler_404_Gruppe_Nicht_gefunden {
 
     private static final double CONSUMER_VAT = 0.19, BASIC_VAT = 0.07;
-    private static int products = 1;
+    private static int products = 0;
     private static double basicVatPrice = 0, consumerVatPrice = 0;
 
     public static void main(String[] args){
-        double productSum;
+        double productSum = 0;
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Wie viele Produkte werden angegeben?\r\n> ");
+        int productCount = scanner.nextInt();
+        System.out.println("\r\n");
+        for (int i = 0; i < productCount; i++) {
+            productSum += getProductPrice(scanner);
+        }
 
-        // 1
-        productSum = getProductPrice(scanner);
-        // 2
-        productSum += getProductPrice(scanner);
-        // 3
-        productSum += getProductPrice(scanner);
-        // 4
-        productSum += getProductPrice(scanner);
-        // 5
-        productSum += getProductPrice(scanner);
         scanner.close();
 
         outputSumAndMWS(productSum);
@@ -51,18 +47,18 @@ public class Fehler_404_Gruppe_Nicht_gefunden {
 
     private static double getConsumerProduct(Scanner scanner) {
         System.out.println("Bitte geben Sie den Werte des Konsumgüterprodukts im folgenden Format ein (5,5 oder 4):");
-        System.out.printf("%d Produkt > ", products++);
+        System.out.printf("%d Produkt > ", products);
         return scanner.nextDouble();
     }
 
     private static double getBasicProduct(Scanner scanner) {
         System.out.println("Bitte geben Sie den Werte des Grundbedarfsprodukts im folgenden Format ein (5,5 oder 4):");
-        System.out.printf("%d Produkt > ", products++);
+        System.out.printf("%d Produkt > ", products);
         return scanner.nextDouble();
     }
 
     private static boolean productIsBasicProduct(Scanner scanner) {
-        System.out.printf("Ist das %d Produkt ein:\r\n1: Grundbedarfsprodukt\r\n2: Konsumgüterprodukt\r\n> ", products);
+        System.out.printf("Ist das %d Produkt ein:\r\n1: Grundbedarfsprodukt\r\n2: Konsumgüterprodukt\r\n> ", ++products);
         String choice = scanner.next();
         System.out.println(choice);
         return choice.contains("1") ||
@@ -74,6 +70,6 @@ public class Fehler_404_Gruppe_Nicht_gefunden {
         System.out.printf("\r\n\r\nSumme: %.2f EUR\r\n" +
                 "Anteil Mehrwertsteuer (Grundbedarf %d%%): %.2f EUR\r\n" +
                 "Anteil Mehrwertsteuer (Konsumgüter %d%%): %.2f EUR\r\n" +
-                "Anzahl der Produkte: 5", sumOfProduct, ((int) BASIC_VAT * 100), basicVatPrice, ((int) CONSUMER_VAT * 100), consumerVatPrice);
+                "Anzahl der Produkte: %d", sumOfProduct, ((int) (BASIC_VAT * 100)), basicVatPrice, ((int) (CONSUMER_VAT * 100)), consumerVatPrice, products);
     }
 }
